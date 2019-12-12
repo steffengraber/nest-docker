e4ba470e4539fadfe4a1c98cbaaea5e3ba679bef
Steffen Graber <s.graber@fz-juelich.de>
2019-12-12 13:00:51 +0100

    first image for Piz Daint

---
+++ src/piz-daint/piz-daint-cuda/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+
+# NEST environment
+source /usr/bin/nest_vars.sh
+
+# Running NEST to test and to copy the .nestrc into /home/nest
+# nest --help
+
+export MUSIC_ROOT_DIR=/usr
+export MUSIC_ROOT=${MUSIC_ROOT_DIR}
+MUSIC_PATH=${MUSIC_ROOT_DIR}
+export LD_LIBRARY_PATH=${MUSIC_PATH}/lib:$LD_LIBRARY_PATH
+export PATH=${MUSIC_PATH}/bin:$PATH
+export CPATH=${MUSIC_PATH}/include:$CPATH
+export PYTHONPATH=${MUSIC_PATH}/lib/python3.6/site-packages:$PYTHONPATH
+
+exec "$@"
