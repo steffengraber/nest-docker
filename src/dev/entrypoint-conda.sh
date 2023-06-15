c6eab5deee21bbbd851435f7140ccae296e33070
Steffen Graber <s.graber@fz-juelich.de>
2023-06-15 08:33:50 +0000

    Disable Music stuff

--- src/dev/entrypoint-conda.sh
+++ src/dev/entrypoint-conda.sh
@@ -12,13 +12,13 @@ conda activate nestenv
 # Running NEST to test and to copy the .nestrc into /home/nest
 nest --help
 
-export MUSIC_ROOT_DIR=/opt/music-install
-export MUSIC_ROOT=${MUSIC_ROOT_DIR}
-MUSIC_PATH=${MUSIC_ROOT_DIR}
-export LD_LIBRARY_PATH=${MUSIC_PATH}/lib:$LD_LIBRARY_PATH
-export PATH=${MUSIC_PATH}/bin:$PATH
-export CPATH=${MUSIC_PATH}/include:$CPATH
-export PYTHONPATH=${MUSIC_PATH}/lib/python3.8/site-packages:$PYTHONPATH
+# export MUSIC_ROOT_DIR=/opt/music-install
+# export MUSIC_ROOT=${MUSIC_ROOT_DIR}
+# MUSIC_PATH=${MUSIC_ROOT_DIR}
+# export LD_LIBRARY_PATH=${MUSIC_PATH}/lib:$LD_LIBRARY_PATH
+# export PATH=${MUSIC_PATH}/bin:$PATH
+# export CPATH=${MUSIC_PATH}/include:$CPATH
+# export PYTHONPATH=${MUSIC_PATH}/lib/python3.8/site-packages:$PYTHONPATH
 
 
 MODE="${NEST_CONTAINER_MODE:-$1}"
